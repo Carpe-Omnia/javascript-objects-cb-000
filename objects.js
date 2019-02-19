945b4d19a20ee8b42655a16ad81bcde0b3a54a67
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 18:33:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -12,3 +12,7 @@ function updatePlaylist(playlist, name, title) {
   playlist[name] = title
   return playlist
 }
+function removeFromPlaylist(playlist, name) {
+  delete playlist[name]
+  return playlist
+}
