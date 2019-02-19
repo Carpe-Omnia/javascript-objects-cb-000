06b98bda2462d0c0e04bd56f3212a6fd09926463
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-19 18:27:54 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
 
   return obj
 }
+function updateObjectWithKeyAndValue(obj, key, value) {
+ 
+  return Object.assign({}, obj, { [key]: value })
+}
